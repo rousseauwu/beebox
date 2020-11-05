144a719dfbfd236a91a43dd584202bc058098ff0
rousseau wu <wu657983699@gmail.com>
2020-11-05 20:55:39 +0800

    no message

--- src/test/java/org/bees/box/KafkaProducerTest.java
+++ src/test/java/org/bees/box/KafkaProducerTest.java
@@ -1,26 +1,34 @@
 package org.bees.box;
 
 
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/config/*/*.xml")
 public class KafkaProducerTest {
     private static Logger logger = LoggerFactory.getLogger(KafkaTest.class);
 
     @Autowired
-    private KafkaTemplate<String,String> template;
+    private Producer kafkaProducer;
 
     @Test
     public void sendMessage(){
-        for (int i = 0; i < 10; i++) {
-            template.send("mytopic",String.valueOf(i));
+        String topic = "mytopic";
+        for (int i = 1; i <= 10; i++) {
+            String value = "value_" + i;
+            ProducerRecord<String, String> msg = new ProducerRecord<String, String>(topic, value);
+            kafkaProducer.send(msg);
         }
+
         logger.info("finished");
     }
 }
